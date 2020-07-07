1da3b094da75831b3837d5847f777499e5e4af5c
sweety-suhane <61380237+sweety-suhane@users.noreply.github.com>
2020-07-06 23:56:41 -0700

    Update local_settings.py

--- sweety/local_settings.py
+++ sweety/local_settings.py
@@ -1,6 +1,17 @@
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DATABASES = {
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+else
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -8,6 +19,6 @@ DATABASES = {
         'USER': 'postgres',
         'PASSWORD': 'sweety123suhane',
         'HOST' : 'localhost',
-        'PORT'  :   '',
+        'PORT'  :   '5432',
     }
 }
