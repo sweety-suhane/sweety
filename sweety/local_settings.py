4e9d5b75cbd07ee5d0b5b97f246aba99e926ade3
sweety-suhane <61380237+sweety-suhane@users.noreply.github.com>
2020-07-06 22:33:01 -0700

    Update local_settings.py

--- sweety/local_settings.py
+++ sweety/local_settings.py
@@ -1,8 +1,13 @@
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATABASES = {
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sweety',
+        'USER': 'postgres',
+        'PASSWORD': 'sweety123suhane',
+        'HOST' : 'localhost',
+        'PORT'  :   '',
     }
 }
